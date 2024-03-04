ef0e3798c02d86a199c90976d8ef18b1ddd31758
Alejandra Codocedo <78942170+Lettarx@users.noreply.github.com>
2024-03-04 12:26:52 -0300

    App svelte: manejo estados

---
+++ src/components/svelte/App.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Alert, P } from 'flowbite-svelte';
+    import { APP_STATUS, appStatus } from '../../store'
+    import StepLoading from './StepLoading.svelte';
+    import StepUpload from './StepUpload.svelte';
+    import StepAccepted from './StepAccepted.svelte';
+    import StepRefused from './StepRefused.svelte';
+</script>
+
+<!-- EL $ en la store es para observar el valor que tienen -->
+
+{#if $appStatus == APP_STATUS.INIT}
+    <StepUpload/>
+{:else if $appStatus == APP_STATUS.LOADING}
+    <StepLoading/>
+{:else if $appStatus == APP_STATUS.ACCEPTED}
+    <StepAccepted/>
+{:else if $appStatus == APP_STATUS.REFUSED}
+    <StepRefused/>
+{:else if $appStatus == APP_STATUS.ERROR}
+    <Alert>
+        <span class="font-medium">Algo malo a pasado!</span>
+        Hay un error en la aplicación
+    </Alert>
+{:else}
+    <Alert>
+        <span class="font-medium">Acción no reconocida</span>
+        Este estado de la aplicación no está conocido
+    </Alert>
+{/if}
+
+
+
---
+++ src/components/svelte/App2.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { Alert, P } from 'flowbite-svelte';
+    import { APP_STATUS2, appStatus2 } from '../../store'
+    import StepLoading from './StepLoading.svelte';
+    import StepUpload from './StepUpload.svelte';
+    import StepAccepted from './StepAccepted.svelte';
+    import StepRefused from './StepRefused.svelte';
+</script>
+
+<!-- EL $ en la store es para observar el valor que tienen -->
+
+{#if $appStatus2 == APP_STATUS2.INIT}
+    <StepUpload/>
+{:else if $appStatus2 == APP_STATUS2.LOADING}
+    <StepLoading/>
+{:else if $appStatus2 == APP_STATUS2.ACCEPTED}
+    <StepAccepted/>
+{:else if $appStatus2 == APP_STATUS2.REFUSED}
+    <StepRefused/>
+{:else if $appStatus2 == APP_STATUS2.ERROR}
+    <Alert>
+        <span class="font-medium">Algo malo a pasado!</span>
+        Hay un error en la aplicación
+    </Alert>
+{:else}
+    <Alert>
+        <span class="font-medium">Acción no reconocida</span>
+        Este estado de la aplicación no está conocido
+    </Alert>
+{/if}
