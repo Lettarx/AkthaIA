d9ca033848ef8eb9e89f7d1bc90b5bf537557698
Alejandra Codocedo <78942170+Lettarx@users.noreply.github.com>
2024-03-05 13:12:49 -0300

    se separaron los procesos
    se separaron todos los procesos para tener el código mas legible y liberar la api

--- src/pages/api/apiM.py
+++ src/pages/api/apiM.py
@@ -1,10 +1,4 @@
-import cv2
-import numpy as np
-import io
-import base64
-from PIL import Image as PILImage
-from fastapi import FastAPI, HTTPException, UploadFile, File
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, File
 from src.functions.procesar import procesarImg, evaluarSimilitud
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -26,160 +20,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/prueba/")
+
+@app.post("/CriteriosEvaluacion/")
 async def evaluar_imagen(file: UploadFile = File(...)):
+
     img = await file.read()
-    image = PILImage.open(io.BytesIO(img))
-    image_np = np.array(image)
-    original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
     
-    #IMG2
-    img = cv2.imread('img1.jpg')
-    image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    # Check if 2 images are equal shape
-    if original.shape == image.shape:
-        difference = cv2.subtract(original, image)
-        b, g, r = cv2.split(difference)
-        if (cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0):
-            print('Las imagenes son completamente iguales')
-            return {"docs": [{"Mach": 100}]}
-
-    # Sift and Flann
-    shift = cv2.xfeatures2d.SIFT_create()
-    kp_1, desc_1 = shift.detectAndCompute(original, None)
-    kp_2, desc_2 = shift.detectAndCompute(image, None)
-
-    index_params = dict(algorithm=0, trees=5)
-    search_params = dict()
-
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(desc_1, desc_2, k=2)
-
-    # 2) Check for similarities between the 2 images
-    good_points = []
-    for m, n in matches:
-        if m.distance < 0.6*n.distance:
-            good_points.append(m)
-
-    number_keypoints = 0
-    if (len(kp_1) <= len(kp_2)):
-        number_keypoints = len(kp_1)
-    else:
-        number_keypoints = len(kp_2)
-
-    percentage_similarity = len(good_points) / number_keypoints * 100
-    mach = "Que tan bueno es el match", percentage_similarity, "%"
-    return {"docs": [{"Mach": percentage_similarity}]}
-
-@app.post("/pruebamodel/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    img = await file.read()
-    image = PILImage.open(io.BytesIO(img))
-    
-    image_np = np.array(image)
-    original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-    return procesarImg(original,'src/modelos/best640.pt')
-
-
-class Image(BaseModel):
-    url: str
-
-
-@app.post("/ImgOscura/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    try:
-        img = await file.read()
-        image = PILImage.open(io.BytesIO(img))
-        model = 'best640.pt'
-        image_np = np.array(image)
-        original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        return procesarImg(original,model)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {str(e)}")
-
-@app.post("/Personas/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    try:
-        img = await file.read()
-        image = PILImage.open(io.BytesIO(img))
-        model = 'Modelos/epoch_249_640_personas.pt'
-        image_np = np.array(image)
-        original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        return procesarImg(original,model)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {str(e)}")
-
-@app.post("/ImgIluminada/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    try:
-        img = await file.read()
-        image = PILImage.open(io.BytesIO(img))
-        model = 'Modelos/best_640_imgiluminadas.pt'
-        image_np = np.array(image)
-        original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        return procesarImg(original,model)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {str(e)}")
-
-
-@app.post("/ImgMovida/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    try:
-        img = await file.read()
-        image = PILImage.open(io.BytesIO(img))
-        model = 'Modelos/best_202_160_imgmovidas.pt'
-        image_np = np.array(image)
-        original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        return procesarImg(original,model)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {str(e)}")
-
-@app.post("/ImgDesenfocada/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    try:
-        img = await file.read()
-        image = PILImage.open(io.BytesIO(img))
-        model = 'Modelos/best_320_blur.pt'
-        image_np = np.array(image)
-        original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        return procesarImg(original,model)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {str(e)}")
-       
-
-@app.post("/ParedesEsquinas/")
-async def evaluar_imagen(file: UploadFile = File(...)):
-    try:
-        img = await file.read()
-        image = PILImage.open(io.BytesIO(img))
-        model = 'Modelos/best_320_paredesesquinas.pt'
-        image_np = np.array(image)
-        original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        return procesarImg(original,model)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {str(e)}")
+    return procesarImg(img)
 
 
 @app.post('/similitud/')
-async def evaluar_silimitud(image:Image, image2:Image):
-    #IMG1
-    imagen_bytes = base64.b64decode(image.url)
-    imagen_np = np.frombuffer(imagen_bytes, dtype=np.uint8)
-    original = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
-    
-    #IMG2
-    imagen_bytes = base64.b64decode(image2.url)
-    imagen_np = np.frombuffer(imagen_bytes, dtype=np.uint8)
-    image = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
-
-    return evaluarSimilitud(original,image)
+async def evaluar_imagen(img1: UploadFile = File(...), img2: UploadFile = File(...)):
+    return evaluarSimilitud(img1,img2)
     
 
 if __name__ == "__main__":
--- src/pages/api/src/functions/procesar.py
+++ src/pages/api/src/functions/procesar.py
@@ -1,7 +1,21 @@
 
 from torchvision.transforms.functional import to_tensor
 import torch
+from fastapi import UploadFile, File, HTTPException
+from PIL import Image as PILImage
+import io
+import cv2
+import numpy as np
 
+
+modelos = {
+    "iluminada"  : 'src/modelos/best640iluminadas.pt',
+    "oscura"     : 'src/modelos/best160oscuras.pt',
+    "movida"     : 'src/modelos/epoch2950640movimiento.pt',
+    "desenfocada": 'src/modelos/best640blur.pt',
+    "pared"      : 'src/modelos/best640blur.pt',
+    "personas"   : 'src/modelos/best2360160personas.pt'
+}
 
 def evaluarDefectos(img, modelo):
     model = torch.hub.load('./', 'custom', modelo, source='local', force_reload=True)
@@ -9,8 +23,10 @@ def evaluarDefectos(img, modelo):
     data = results.pandas().xyxy[0]
     return data
 
-def procesarImg(img, modelo):
-    data = evaluarDefectos(img, modelo)
+def unirModelos():
+    pass
+
+def formatearData(data):
     # Procesar los resultados
     confidence = data['confidence']
     label = data['name']
@@ -23,9 +39,51 @@ def procesarImg(img, modelo):
             "confidence": round(confidence[i] * 100, 1)
         })
 
-    return {"docs": resultados_formateados}
+    return resultados_formateados
 
-def evaluarSimilitud(img1,img2):
+
+
+def procesarImg(img):
+    #Preparar imagen
+    image = PILImage.open(io.BytesIO(img))
+    image_np = np.array(image)
+    imgLista = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+
+    #Evaluar con el modelo correspondiente
+    dataIluminada   = evaluarDefectos(imgLista, modelos.get('iluminada'))
+    dataOscura      = evaluarDefectos(imgLista, modelos.get('oscura'))
+    dataMovida      = evaluarDefectos(imgLista, modelos.get('movida'))
+    dataDesenfocada = evaluarDefectos(imgLista, modelos.get('desenfocada'))
+    dataPared       = evaluarDefectos(imgLista, modelos.get('pared'))
+    dataPersonas    = evaluarDefectos(imgLista, modelos.get('personas'))
+
+    # Crear la estructura de salida
+    output = {
+        "docs": {
+            "iluminada"  : formatearData(dataIluminada),
+            "oscura"     : formatearData(dataOscura),
+            "movida"     : formatearData(dataMovida),
+            "desenfocada": formatearData(dataDesenfocada),
+            "pared"      : formatearData(dataPared),
+            "personas"   : formatearData(dataPersonas)
+        }
+    }
+
+    return output
+    
+
+
+def evaluarSimilitud(i1,i2):
+    #preparar imagen
+    image = PILImage.open(io.BytesIO(i1))
+    image_np = np.array(image)
+    img1 = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+
+    image = PILImage.open(io.BytesIO(i2))
+    image_np = np.array(image)
+    img2 = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+
+
     # Check if 2 images are equal shape
     if img1.shape == img2.shape:
         difference = cv2.subtract(img1, img2)
