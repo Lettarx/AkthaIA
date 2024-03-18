94b4ddeec557c295cd8ab0b502febf5a0f1477ab
Alejandra Codocedo <78942170+Lettarx@users.noreply.github.com>
2024-03-18 18:56:28 -0300

    separar logica rechazo y aprobación
    en el estatus rechazo solo para mostrar el porque se rechazo y redirigirte denuevo al upload, aceptación es solo para informar que se acepto la imagen

---
+++ src/components/svelte/StepAccepted.svelte
@@ -0,0 +1,8 @@
+<div class=" mb-10 aspect-auto flex flex-grow justify-center items-center p-3 border-2 border-dashed border-gray-800 bg-slate-600 text-white outline-none transition duration-300 focus:border-blue-600">
+    <svg xmlns="http://www.w3.org/2000/svg"  width="55" height="55" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M9 12l2 2l4 -4" />
+        <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+    </svg>
+    <p>Aprobada</p>
+</div>
---
+++ src/components/svelte/StepRefused.svelte
@@ -0,0 +1,117 @@
+<script>
+    import max from './StepAnalyze.svelte'
+</script>
+
+<!-- 
+
+  <div class="pb-4">
+    {#if desenfocada.length > 0}
+        <h3><strong>Imágenes desenfocadas:</strong></h3>
+        {#each desenfocada as resultado}
+                {#if resultado.confidence === Math.max(...desenfocada.map(p => p.confidence))}
+                    {#if resultado.label === "Focus"}
+                        <p>No esta desenfocada</p>
+                    {:else}
+                    <table class="table-auto">
+                        <tr>
+                        <th>Label</th>
+                        <th>Confidence</th>
+                        </tr>
+                        <tr>
+                            <td>{resultado.label}</td>
+                            <td>{resultado.confidence}%</td>
+                        </tr>
+                    </table> 
+                    {/if}
+                {/if}
+        {/each}
+    {:else}
+        <p>No hay resultados para imágenes desenfocadas.</p>
+    {/if}
+  </div>
+  
+  <div class="pb-4">
+    {#if movida.length > 0}
+        <h3><strong>Imágenes movidas:</strong></h3>
+        {#each movida as resultado}
+                {#if resultado.confidence === Math.max(...movida.map(p => p.confidence))}
+                    {#if resultado.label === "Not blurry"}
+                        <p>No esta movida</p>
+                    {:else}
+                    <table class="table-auto">
+                        <tr>
+                        <th>Label</th>
+                        <th>Confidence</th>
+                        </tr>
+                        <tr>
+                            <td>{resultado.label}</td>
+                            <td>{resultado.confidence}%</td>
+                        </tr>
+                    </table> 
+                    {/if}
+                {/if}
+        {/each}
+    {:else}
+        <p>No hay resultados para imágenes movidas.</p>
+    {/if}
+  </div>
+
+  <div class="pb-4">
+    {#if oscura.length > 0}
+        <h3><strong>Imágenes oscuras:</strong></h3>
+        {#each oscura as resultado}
+                {#if resultado.confidence === Math.max(...oscura.map(p => p.confidence))}
+                    {#if resultado.label === "Lit"}  revisar la etiqueta LightDark 
+                        <p>No esta oscura</p>
+                    {:else}
+                    <table class="table-auto">
+                        <tr>
+                        <th>Label</th>
+                        <th>Confidence</th>
+                        </tr>
+                        <tr>
+                            <td>{resultado.label}</td>
+                            <td>{resultado.confidence}%</td>
+                        </tr>
+                    </table> 
+                    {/if}
+                {/if}
+        {/each}
+    {:else}
+        <p>No hay resultados para imágenes oscuras.</p>
+    {/if}
+  </div>
+
+  <div class="pb-4">
+    {#if pared.length > 0}
+        <h3><strong>Imágenes con paredes:</strong></h3>
+        {#each pared as resultado}
+                {#if resultado.confidence === Math.max(...pared.map(p => p.confidence))}
+                    {#if resultado.label === ""}  revisar lo de la esquina, pared o rincon 
+                        <p>No tiene pared o esquina</p>
+                    {:else}
+                    <p>Hay una pared en un {resultado.confidence}%</p>
+                     <table class="table-auto">
+                        <tr>
+                        <th>Label</th>
+                        <th>Confidence</th>
+                        </tr>
+                        <tr>
+                            <td>{resultado.label}</td>
+                            <td>{resultado.confidence}%</td>
+                        </tr>
+                    </table>  
+                    {/if}
+                {/if}
+        {/each}
+    {:else}
+        <p>No hay resultados para imágenes con paredes.</p>
+    {/if}
+  </div> -->
+
+<div class="pb-4">
+    <h3><strong>Imágenes con personas:</strong></h3>
+    <p>hay personas en un  {max.personas}%</p>
+                
+</div> 
+
